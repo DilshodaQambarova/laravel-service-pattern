75510f189538d9f5c7376594edff6b7bfd64dde9
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-06 21:28:34 +0500

    Added fillable and relation method to model Attachment

--- app/Models/Attachment.php
+++ app/Models/Attachment.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Attachment extends Model
 {
-    //
+    protected $fillable = ['path'];
+    public function attachmentable(){
+        return $this->morphTo();
+    }
 }
