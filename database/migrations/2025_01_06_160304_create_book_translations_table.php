223bcd2f724c335813bfa8dcbaf57b57965c9fa1
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-06 21:03:33 +0500

    Added new model and migration for book translation

--- database/migrations/2025_01_06_160304_create_book_translations_table.php
+++ database/migrations/2025_01_06_160304_create_book_translations_table.php
@@ -11,9 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('books', function (Blueprint $table) {
+        Schema::create('book_translations', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -23,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('books');
+        Schema::dropIfExists('book_translations');
     }
 };
