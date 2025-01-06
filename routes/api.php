eb8791e45f0b70d66d8f8e15ec0ad69fe080bdbf
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-06 23:55:29 +0500

    Added routes for authhentication & book

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('setLocale')->group(function(){
+    Route::middleware('auth:sanctum')->group(function(){
+        Route::get('/user', [AuthController::class, 'findUser']);
+        Route::apiResource('/books', BookController::class);
+    });
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+});
