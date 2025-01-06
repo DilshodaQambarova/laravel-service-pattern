46da1b839c771dd43e0f2942d1aa25bd21334472
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-06 22:24:47 +0500

    Created repositories, services and DTOs for book and user

---
+++ app/DTO/BookDTO.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\DTO;
+
+class BookDTO
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
---
+++ app/Services/BookService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+class BookService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
