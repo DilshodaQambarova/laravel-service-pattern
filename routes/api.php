86827ed1ad4fa8da9deef458fc528e459d7e6596
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-09 20:22:59 +0500

    Added route for filter

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,7 @@ Route::middleware('setLocale')->group(function(){
     Route::middleware('auth:sanctum')->group(function(){
         Route::get('/user', [AuthController::class, 'findUser']);
         Route::get('/logout', [AuthController::class, 'logout']);
+        Route::get('/filter-book', [BookController::class, 'filterBook']);
         Route::apiResource('/books', BookController::class);
     });
     Route::post('/register', [AuthController::class, 'register']);
