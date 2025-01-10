3999fe8c4ba78350c15dcc9aff6321fc1674406a
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-10 21:38:51 +0500

    Added route for search

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,7 @@ Route::middleware('setLocale')->group(function(){
         Route::get('/user', [AuthController::class, 'findUser']);
         Route::get('/logout', [AuthController::class, 'logout']);
         Route::get('/filter-book', [BookController::class, 'filterBook']);
+        Route::get('/search', [BookController::class, 'search']);
         Route::apiResource('/books', BookController::class);
     });
     Route::post('/register', [AuthController::class, 'register']);
