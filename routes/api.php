f63157f42e71fa2d8619470de645927774922c04
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-13 13:02:55 +0500

    Removed search and filter route

--- routes/api.php
+++ routes/api.php
@@ -9,8 +9,6 @@ Route::middleware('setLocale')->group(function(){
     Route::middleware('auth:sanctum')->group(function(){
         Route::get('/user', [AuthController::class, 'findUser']);
         Route::get('/logout', [AuthController::class, 'logout']);
-        Route::get('/filter-book', [BookController::class, 'filterBook']);
-        Route::get('/search', [BookController::class, 'search']);
         Route::apiResource('/books', BookController::class);
     });
     Route::post('/register', [AuthController::class, 'register']);
